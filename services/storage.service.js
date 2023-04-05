5fecc0201095a06d50ab25c334d5370decf9700d
Lykov Ilya <ilya.lykov.program@gmail.com>
2023-04-05 13:38:15 +0400

    add: save token feature

--- services/storage.service.js
+++ services/storage.service.js
@@ -1,16 +1,32 @@
-import { homedir } from "os";
-import { join, basename, dirname, extname, relative, isAbsolute, resolve, sep } from "path";
+import { promises } from "fs";
+import { join } from "path";
 
-const filePath = join(homedir(), "weather-data-json.json");
-const saveKeyValue = (key, value) => {
-	console.log(basename(filePath));
-	console.log(dirname(filePath));
-	console.log(extname(filePath));
-	console.log(relative(filePath, dirname(filePath)));
-	console.log(isAbsolute(filePath));
-	console.log(resolve(".."));
-	console.log(sep);
-
+const filePath = join("D:\\nodeJS_projects\\weather-cli\\db", "weather-data.json");
+const saveKeyValue = async (key, value) => {
+	let data = {};
+	if (await isExist(filePath)) {
+		const file = await promises.readFile(filePath);
+		data = JSON.parse(file);
+	}
+	data[key] = value;
+	await promises.writeFile(filePath, JSON.stringify(data));
+};
+const getKeyValue = async (key) => {
+	if (await isExist(filePath)) {
+		const file = await promises.readFile(filePath);
+		const data = JSON.parse(file);
+		return data[key];
+	}
+	return undefined;
 };
 
-export { saveKeyValue };
+const isExist = async (path) => {
+	try {
+		await promises.stat(path);
+		return true;
+	} catch (err) {
+		return false;
+	}
+};
+
+export { saveKeyValue, getKeyValue };
