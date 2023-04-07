ad964a78cc2611e8a114a5d27c57790ba70ce343
Lykov Ilya <ilya.lykov.program@gmail.com>
2023-04-07 11:16:20 +0400

    add: weather output & and complite feature for save CITY in jsonDB

--- services/api.service.js
+++ services/api.service.js
@@ -3,8 +3,9 @@ import { printError } from "./log.service.js";
 import axios from "axios";
 
 
-const getWeather = async (city) => {
+const getWeather = async () => {
 	const token = process.env.TOKEN ?? await getKeyValue(TOKEN_DICTIONARY.token);
+	const city = process.env.CITY ?? await getKeyValue(TOKEN_DICTIONARY.city);
 	if (!token) {
 		throw new Error("Не задан ключ API, задайте его через команду -t [API-KEY].");
 	}
--- weather.js
+++ weather.js
@@ -19,11 +19,23 @@ const saveToken = async (token) => {
 		printError(err.message);
 	}
 };
+const saveCity = async (city) => {
+	if (!city.length) {
+		printError("Не передан город");
+		return;
+	}
+	try {
+		await saveKeyValue(TOKEN_DICTIONARY.city, city);
+		printSuccess("Город Сохранен");
+	} catch (err) {
+		printError(err.message);
+	}
+};
 
 const getForcast = async () => {
 	try {
 		const weather = await getWeather(process.env.CITY);
-		console.log(weather);
+		printWeather(weather);
 	} catch (e) {
 		if (e?.response?.status == 404) {
 			printError("Неверно указан город");
@@ -32,7 +44,6 @@ const getForcast = async () => {
 		} else {
 			printError(e.message);
 		}
-
 	}
 
 };
@@ -43,11 +54,27 @@ const initCLI = () => {
 		printHelp();
 	}
 	if (args.s) {
+		return saveCity(args.s);
 	}
 	if (args.t) {
 		return saveToken(args.t);
 	}
 	getForcast();
+};
+
+const printWeather = (weather) => {
+	const {
+		weather: [{
+			description
+		}],
+		main: {
+			temp,
+			temp_min,
+			temp_max,
+		},
+		name
+	} = weather;
+	console.log(`${name}, погода:\n${description}\nТекущая температура: ${temp}C\n`);
 };
 
 initCLI();
